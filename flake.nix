823b550c47367f9bd901a0b343821d6186cadd2b
Marcelo Lima <m-lima@users.noreply.github.com>
2026-03-26 17:06:24 +0100

    [zig] Building

--- flake.nix
+++ flake.nix
@@ -1,23 +1,86 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs = {
+        systems.follows = "systems";
+      };
+    };
+    systems.url = "github:nix-systems/default";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
     };
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
   };
 
   outputs =
     {
-      helper,
+      nixpkgs,
+      flake-utils,
+      zig,
       ...
-    }@inputs:
-    helper.lib.go.helper inputs ./. {
-      pname = "wifidog";
-      version = "0.0.1";
-      devPackages = pkgs: [ pkgs.zig ];
-    };
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+        zigPkg = zig.packages.${system}.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            zigPkg
+            pkgs.zls
+          ];
+        };
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "wifidog";
+          version = "0.0.1";
+
+          src =
+            let
+              fs = lib.fileset;
+            in
+            fs.toSource {
+              root = ./.;
+              fileset = fs.intersection (fs.fromSource (lib.sources.cleanSource ./.)) (
+                fs.unions [
+                  ./src
+                  ./build.zig
+                  ./build.zig.zon
+                ]
+              );
+            };
+
+          nativeBuildInputs = [
+            zigPkg
+          ];
+
+          dontInstall = true;
+
+          configurePhase = ''
+            runHook preConfigure
+            export ZIG_GLOBAL_CACHE_DIR=$TEMP/.cache
+            runHook postConfigure
+          '';
+
+          buildPhase = ''
+            runHook preBuild
+            zig build install --color off --prefix $out
+            runHook postBuild
+          '';
+        };
+      }
+    );
 }
