59f5e3668d87d47ec446f5652ce083f1c4d868fd
Marcelo Lima <m-lima@users.noreply.github.com>
2026-03-26 17:25:33 +0100

    [zig] Unify dependencies

--- flake.nix
+++ flake.nix
@@ -1,13 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs = {
-        systems.follows = "systems";
-      };
-    };
-    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs = {
@@ -18,7 +12,7 @@
       url = "github:mitchellh/zig-overlay";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
+        systems.follows = "flake-utils/systems";
       };
     };
   };
