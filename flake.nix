fb5c055b25711718493551bc32a1e907b5eb2ccd
Marcelo Lima <m-lima@users.noreply.github.com>
2026-03-26 17:49:39 +0100

    [zig] Working

--- flake.nix
+++ flake.nix
@@ -19,8 +19,10 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
       zig,
       ...
     }:
@@ -30,36 +32,59 @@
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
         zigPkg = zig.packages.${system}.default;
+
+        fmtConfig = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixfmt.enable = true;
+            yamlfmt.enable = true;
+            zig.enable = true;
+          };
+          settings = {
+            on-unmatched = "warn";
+            excludes = [
+              "**/.direnv/*"
+              "**/.envrc"
+              "**/.gitignore"
+              "*.lock"
+              ".direnv/*"
+              ".envrc"
+              ".git-crypt/*"
+              ".gitattributes"
+              ".gitignore"
+              ".zig-cache/*"
+              "LICENSE"
+              "result*/*"
+              "zig-build/*"
+            ];
+          };
+
+        };
+
+        treefmt = (treefmt-nix.lib.evalModule pkgs fmtConfig).config.build;
+        src =
+          let
+            fs = lib.fileset;
+          in
+          fs.toSource {
+            root = ./.;
+            fileset = fs.intersection (fs.fromSource (lib.sources.cleanSource ./.)) (
+              fs.unions [
+                ./src
+                ./build.zig
+                ./build.zig.zon
+              ]
+            );
+          };
       in
       {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            zigPkg
-            pkgs.zls
-          ];
-        };
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "wifidog";
           version = "0.0.1";
 
-          src =
-            let
-              fs = lib.fileset;
-            in
-            fs.toSource {
-              root = ./.;
-              fileset = fs.intersection (fs.fromSource (lib.sources.cleanSource ./.)) (
-                fs.unions [
-                  ./src
-                  ./build.zig
-                  ./build.zig.zon
-                ]
-              );
-            };
+          inherit src;
 
-          nativeBuildInputs = [
-            zigPkg
-          ];
+          nativeBuildInputs = [ zigPkg ];
 
           dontInstall = true;
 
@@ -74,6 +99,40 @@
             zig build install --color off --prefix $out
             runHook postBuild
           '';
+        };
+
+        checks = {
+          formatting = treefmt.check self;
+          test = pkgs.stdenv.mkDerivation {
+            name = "test";
+
+            inherit src;
+
+            nativeBuildInputs = [ zigPkg ];
+
+            dontBuild = true;
+            doCheck = true;
+            dontInstall = true;
+
+            configurePhase = ''
+              runHook preConfigure
+              export ZIG_GLOBAL_CACHE_DIR=$TEMP/.cache
+              runHook postConfigure
+            '';
+
+            checkPhase = "zig build test";
+            installPhase = "mkdir $out";
+          };
+        };
+
+        formatter = treefmt.wrapper;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            zigPkg
+            pkgs.zls
+            # pkgs.zig-zlint
+          ];
         };
       }
     );
